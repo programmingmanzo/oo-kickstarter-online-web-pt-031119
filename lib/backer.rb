44475e5c142c344ec4fcf44bd4a27de596c2f166
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-16 06:50:48 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Backer 
+
+  attr_reader :backed_projects
+
+  def initialize(name)
+    @name = name 
+    @backed_projects = []
+  end 
+  
+  def name 
+    @name 
+  end 
+
+  def back_project(project)
+    @backed_projects << project 
+    project.add_backer(self) unless project.backers.include?(self)
+    #binding.pry 
+  end
+  
+end 
+
+
+
+
---
+++ lib/project.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+
+class Project 
+
+  attr_reader :backers
+
+  def initialize(title)
+    @title = title 
+   @backers = []
+  end 
+  
+  def title 
+    @title
+  end 
+
+  def add_backer(backer)
+   @backers << backer 
+   backer.back_project(self) unless backer.backed_projects.include?(self)
+  end 
+  
+  
+end 
+
+
+
+
