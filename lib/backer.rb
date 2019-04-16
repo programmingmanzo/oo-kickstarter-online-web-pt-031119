29c989d0bbbf4f56b0e7c19bfb784f28996ab5fa
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-16 06:16:03 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,14 +2,14 @@ require 'pry'
 
 class Backer 
 
-attr_reader :backed_projects
+  attr_reader :backed_projects
 
-def initialize
-  @backed_projects = []
-end 
+  def initialize
+    @backed_projects = []
+  end 
 
-def backed_project(project)
-  @backed_projects << self.project 
+  def backed_project(project)
+    @backed_projects << self.project 
 end 
 
 
